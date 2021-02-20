03488e7353ccec4cb0c1d38b69cf3ed9a6d4eda5
nasos <soulisathanasios@gmail.com>
2021-02-20 16:01:31 +0200

    login form

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+    <router-view />
+
+    <div
+      class="md-layout md-alignment-center-center"
+      style="height: 100vh;"
+    >
+
+    <form novalidate class="md-layout" @submit.prevent="validateUser">
+      <md-card md-with-hover style="padding:100px;">
+        <md-ripple>
+          <md-card-header>
+            <div class="md-title">Login</div>
+          </md-card-header>
+
+          <md-card-content>
+            <md-field>
+              <label>Username</label>
+              <md-input v-model="email"></md-input>
+            </md-field>
+
+            <md-field>
+              <label>Password </label>
+              <md-input v-model="password" type="password"></md-input>
+            </md-field>
+          </md-card-content>
+
+          <md-card-actions clas="md-alignment-left-left" >
+            <md-button v-on:click="login" class="md-primary"  >συνδεση</md-button>
+          </md-card-actions>
+        </md-ripple>
+      </md-card>
+    </form>
+
+
+    </div>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -23,6 +49,62 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
+
+
+<script>
+import axios from "axios";
+import environment from "./environment";
+export default {
+  
+data(){
+  return{
+    email:'',
+    password:'',
+  };
+
+},
+methods: {
+
+  login(){
+    console.log("dd");
+
+      axios
+        .post(`${environment.baseUrl}/api/login`, {
+         
+            email: this.email,
+            password: this.password,
+            
+         
+        })
+        .then(() => {
+          console.log("ok");
+
+        })
+        .catch(() => {
+          alert("connect error");
+        });
+
+
+
+  }
+
+},
+
+
+}
+</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
